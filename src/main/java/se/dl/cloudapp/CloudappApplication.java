817be333c0c1874215786fbee2fb42e900fb6d53
Dennis <dennis_lundberg@yahoo.se>
2017-06-16 08:21:12 +0200

    Put it into the cloud... first try

--- src/main/java/se/dl/cloudapp/CloudappApplication.java
+++ src/main/java/se/dl/cloudapp/CloudappApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CloudappApplication {
-
+//TEST
 	public static void main(String[] args) {
 		SpringApplication.run(CloudappApplication.class, args);
 	}
